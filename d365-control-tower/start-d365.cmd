1be7d68288bf81bb587f36b26b2b14e185b8f854
pierceyalex5-star <pierceyalex5@gmail.com>
2026-08-14 06:38:46 -0400

    Remove local server launcher from GitHub Pages D365 deployment

--- d365-control-tower/start-d365.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d %~dp0
-echo Updating D365 Control Tower from GitHub...
-git pull --ff-only
-if errorlevel 1 echo WARNING: Git pull did not complete. Starting the current local version.
-echo.
-echo Starting IFAST D365 Transformation Control Tower...
-node server.js

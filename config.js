32c3fff50e5a7196d7b0561afb0da52d7b4c4862
pierceyalex5-star <pierceyalex5@gmail.com>
2026-08-13 21:30:12 -0400

    Refresh Pages for correction KPI methodology v2

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Finance Control Tower - GitHub shared-state mode
-// Pages refresh: journal KPI analytics + Finance SOP import/open workflow
+// Pages refresh: journal KPI analytics v2 + Finance SOP import/open workflow
 window.FINANCE_API_BASE = "";
